61de7d538a8d69a22979c1b15f9e896d6bec225c
lx9203 <lx9203@naver.com>
2019-03-14 10:38:47 +0900

    First files

---
+++ ch01/src/ch01/Hello.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		System.out.println("안녕? 하세요? 자바? 월드?");
+	}
+
+}
