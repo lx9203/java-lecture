1bac44a91368cc520e1b29c8d979cf3a104c5d61
lx9203 <lx9203@naver.com>
2019-03-15 14:31:40 +0900

    ch04.sec02

---
+++ ch04/src/sec02/SwitchCharExample.java
@@ -0,0 +1,23 @@
+package sec02;
+
+public class SwitchCharExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char grade = 'B';
+		
+		switch(grade) {
+		case 'A':
+		case 'a':
+			System.out.println("우수회원입니다.");
+			break;
+		case 'B':
+		case 'b':
+			System.out.println("일반회원입니다.");
+			break;
+		default :
+			System.out.println("손놈입니다.");
+		}
+	}
+
+}
---
+++ ch04/src/sec02/SwitchStringExanple.java
@@ -0,0 +1,25 @@
+package sec02;
+
+import java.util.Scanner;
+
+public class SwitchStringExanple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.print("직급을 입력하세요. > ");
+		String position = scan.nextLine();
+		
+		switch(position) {
+		case "부장" :
+			System.out.println("700만원");
+			break;
+		case "과장" :
+			System.out.println("500만원");
+			break;
+		default :
+			System.out.println("300만원");
+		}
+	}
+
+}
