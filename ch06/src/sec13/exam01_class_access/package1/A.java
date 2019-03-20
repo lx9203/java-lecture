39b9741525dba3b219b1324889f6161648499a59
lx9203 <lx9203@naver.com>
2019-03-20 11:53:24 +0900

    ch06

---
+++ ch06/src/sec13/exam01_class_access/package1/A.java
@@ -0,0 +1,3 @@
+package sec13.exam01_class_access.package1;
+
+class A {}
---
+++ ch06/src/sec13/exam01_class_access/package2/C.java
@@ -0,0 +1,8 @@
+package sec13.exam01_class_access.package2;
+
+import sec13.exam01_class_access.package1.*;
+
+public class C {
+//	A a;
+	B b;
+}
