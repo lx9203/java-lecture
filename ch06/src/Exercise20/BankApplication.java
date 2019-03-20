342da1bf68aa1e67d37f6999067bdc64f6c56b62
lx9203 <lx9203@naver.com>
2019-03-20 17:46:33 +0900

    ch06

--- ch06/src/Exercise20/BankApplication.java
+++ ch06/src/Exercise20/BankApplication.java
@@ -76,10 +76,48 @@ public class BankApplication {
 	}
 
 	private static void deposit() {
+		System.out.println("--------------");
+		System.out.println("예금");
+		System.out.println("--------------");
+		System.out.print("계좌번호: ");
+		String ano = scan.nextLine();
+		System.out.print("예금액: ");
+		int money = Integer.parseInt(scan.nextLine());
+		Account account = findAccount(ano);
+		if (account == null) {
+			System.out.println("결과: 계좌가 없습니다.");
+			return;
+		}
+		account.setBalance(account.getBalance() + money);
+		System.out.println("결과: 예금이 성공되었습니다.");
 
 	}
 
 	private static void withdraw() {
-
+		System.out.println("--------------");
+		System.out.println("출금");
+		System.out.println("--------------");
+		System.out.print("계좌번호: ");
+		String ano = scan.nextLine();
+		System.out.print("출금액: ");
+		int money = Integer.parseInt(scan.nextLine());
+		Account account = findAccount(ano);
+		if (account == null) {
+			System.out.println("결과: 계좌가 없습니다.");
+			return;
+		}
+		account.setBalance(account.getBalance() - money);
+		System.out.println("결과: 출금이 성공되었습니다.");
 	}
+	private static Account findAccount(String ano) {
+		for(Account account: accountArray) {
+			if(account == null)
+				break;
+			if(ano.equals(account.getAno())) {
+				return account;
+			}
+		}
+		return null;
+	}
+
 }
