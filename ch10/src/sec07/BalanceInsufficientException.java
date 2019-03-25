87ac47e3350a51ac18c2832db842b813395bde54
lx9203 <lx9203@naver.com>
2019-03-25 12:12:41 +0900

    ch10

---
+++ ch10/src/sec07/BalanceInsufficientException.java
@@ -0,0 +1,11 @@
+package sec07;
+
+public class BalanceInsufficientException extends Exception {
+
+	public BalanceInsufficientException() {
+	}
+
+	public BalanceInsufficientException(String message) {
+		super(message);
+	}
+}
---
+++ ch10/src/sec08/AccountExample.java
@@ -0,0 +1,23 @@
+package sec08;
+
+import sec07.*;
+
+public class AccountExample {
+
+	public static void main(String[] args) {
+		Account account = new Account();
+
+		account.deposit(10000);
+		System.out.println("예금액 : " + account.getBalance());
+
+		try {
+			account.withdraw(30000);
+		} catch (BalanceInsufficientException e) {
+			String message = e.getMessage();
+			System.out.println(message);
+			System.out.println();
+			e.printStackTrace();
+		}
+	}
+
+}
