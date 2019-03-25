e58ae09d55d3819734071c0441dab04c33c799fd
lx9203 <lx9203@naver.com>
2019-03-25 12:13:03 +0900

    Exercise

---
+++ ch10/src/Exercise/NotExistIdException.java
@@ -0,0 +1,9 @@
+package Exercise;
+
+public class NotExistIdException extends Exception {
+	public NotExistIdException() {}
+	public NotExistIdException(String message) {
+		super(message);
+		
+	}
+}
