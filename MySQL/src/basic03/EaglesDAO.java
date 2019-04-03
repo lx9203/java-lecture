6382b91a68f53d3fcd7b442bf66f2d0c81a1fb69
lx9203 <lx9203@naver.com>
2019-04-03 17:04:27 +0900

    basic03

---
+++ MySQL/src/basic03/EaglesDAO.java
@@ -0,0 +1,259 @@
+package basic03;
+
+import java.sql.*;
+import java.util.*;
+
+public class EaglesDAO {
+	private Connection conn;
+	private static final String USERNAME = "javauser";
+	private static final String PASSWORD = "javapass";
+	private static final String URL = "jdbc:mysql://localhost:3306/world?verifyServerCertificate=false&useSSL=false";
+
+	public EaglesDAO() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public EaglesDTO selectOne(int no) {
+		String query = "select * from Eagles where no=?;";
+		PreparedStatement pStmt = null;
+		EaglesDTO eagles = new EaglesDTO();
+		try {
+			pStmt = conn.prepareStatement(query);
+			pStmt.setInt(1, no);
+			ResultSet rs = pStmt.executeQuery();
+
+			while (rs.next()) {
+				eagles.setNo(rs.getInt("no"));
+				eagles.setName(rs.getString("name"));
+				eagles.setPosit(rs.getString("posit"));
+				eagles.setHs(rs.getString("high_school"));
+				eagles.setHand(rs.getString("hand"));
+				eagles.setNickname(rs.getString("nickname"));
+				eagles.setBackNo(rs.getInt("back_No"));
+				eagles.setBirth(rs.getString("birth"));
+				eagles.setSalary(rs.getInt("salary"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pStmt != null && !pStmt.isClosed())
+					pStmt.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+		return eagles;
+	}
+
+	public List<EaglesDTO> selectPlayersByPosition(String posit) {
+		String sql = "select * from eagles where posit like '" + posit + "';";
+		List<EaglesDTO> playerList = selectCondition(sql);
+		return playerList;
+	}
+
+	public List<EaglesDTO> selectPlayersBySalary(int salary) {
+		String sql = "select * from eagles where salary >=" + salary + ";";
+		List<EaglesDTO> playerList = selectCondition(sql);
+		return playerList;
+	}
+
+	public List<EaglesDTO> selectPlayersAll() {
+		String sql = "select * from eagles;";
+		List<EaglesDTO> playerList = selectCondition(sql);
+		return playerList;
+	}
+
+	public List<EaglesDTO> selectCondition(String query) {
+		// String query = "select * from eagles;";
+		PreparedStatement pStmt = null;
+		List<EaglesDTO> list = new ArrayList<EaglesDTO>();
+		try {
+			pStmt = conn.prepareStatement(query);
+			ResultSet rs = pStmt.executeQuery();
+
+			while (rs.next()) {
+				EaglesDTO eagles = new EaglesDTO();
+				eagles.setNo(rs.getInt("no"));
+				eagles.setName(rs.getString("name"));
+				eagles.setPosit(rs.getString("posit"));
+				eagles.setHs(rs.getString("high_school"));
+				eagles.setHand(rs.getString("hand"));
+				eagles.setNickname(rs.getString("nickname"));
+				eagles.setBackNo(rs.getInt("back_No"));
+				eagles.setBirth(rs.getString("birth"));
+				eagles.setSalary(rs.getInt("salary"));
+
+				list.add(eagles);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pStmt != null && !pStmt.isClosed())
+					pStmt.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+		return list;
+	}
+	// public List<EaglesDTO> selectPlayersBysalary(int salary) {
+	// String query = "select * from eagles where salary>=" + salary + ";";
+	// PreparedStatement pStmt = null;
+	// List<EaglesDTO> list = new ArrayList<EaglesDTO>();
+	// try {
+	// pStmt = conn.prepareStatement(query);
+	// ResultSet rs = pStmt.executeQuery();
+	//
+	// while (rs.next()) {
+	// EaglesDTO eagles = new EaglesDTO();
+	// eagles.setNo(rs.getInt("no"));
+	// eagles.setName(rs.getString("name"));
+	// eagles.setPosit(rs.getString("posit"));
+	// eagles.setHs(rs.getString("high_school"));
+	// eagles.setHand(rs.getString("hand"));
+	// eagles.setNickname(rs.getString("nickname"));
+	// eagles.setBackNo(rs.getInt("back_No"));
+	// eagles.setBirth(rs.getString("birth"));
+	// eagles.setSalary(rs.getInt("salary"));
+	//
+	// list.add(eagles);
+	// }
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// } finally {
+	// try {
+	// if (pStmt != null && !pStmt.isClosed())
+	// pStmt.close();
+	// } catch (SQLException se) {
+	// se.printStackTrace();
+	// }
+	// }
+	// return list;
+	// }
+	// public List<EaglesDTO> selectPlayers2() {
+	// String query = "select * from eagles where posit like '외야;";
+	// PreparedStatement pStmt = null;
+	// List<EaglesDTO> list = new ArrayList<EaglesDTO>();
+	// try {
+	// pStmt = conn.prepareStatement(query);
+	// ResultSet rs = pStmt.executeQuery();
+	//
+	// while (rs.next()) {
+	// EaglesDTO eagles = new EaglesDTO();
+	// eagles.setNo(rs.getInt("no"));
+	// eagles.setName(rs.getString("name"));
+	// eagles.setPosit(rs.getString("posit"));
+	// eagles.setHs(rs.getString("high_school"));
+	// eagles.setHand(rs.getString("hand"));
+	// eagles.setNickname(rs.getString("nickname"));
+	// eagles.setBackNo(rs.getInt("back_No"));
+	// eagles.setBirth(rs.getString("birth"));
+	// eagles.setSalary(rs.getInt("salary"));
+	//
+	// list.add(eagles);
+	// }
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// } finally {
+	// try {
+	// if (pStmt != null && !pStmt.isClosed())
+	// pStmt.close();
+	// } catch (SQLException se) {
+	// se.printStackTrace();
+	// }
+	// }
+	// return list;
+	// }
+
+	public void insertEagles(EaglesDTO eagles) {
+		String query = "insert into eagles values (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+		PreparedStatement pStmt = null;
+		try {
+			pStmt = conn.prepareStatement(query);
+			pStmt.setInt(1, eagles.getNo());
+			pStmt.setString(2, eagles.getName());
+			pStmt.setString(3, eagles.getPosit());
+			pStmt.setString(4, eagles.getHs());
+			pStmt.setString(5, eagles.getHand());
+			pStmt.setString(6, eagles.getNickname());
+			pStmt.setInt(7, eagles.getBackNo());
+			pStmt.setString(8, eagles.getBirth());
+			pStmt.setInt(9, eagles.getSalary());
+
+			pStmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pStmt != null && !pStmt.isClosed())
+					pStmt.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+	}
+
+	public void updateEagles(EaglesDTO eagles) {
+		String query = "update eagles set name=?, posit=?, high_school=?, hand=?, nickname=?, back_no=?, birth=?, salary=? where no=?;";
+		PreparedStatement pStmt = null;
+		try {
+			pStmt = conn.prepareStatement(query);
+			pStmt.setString(1, eagles.getName());
+			pStmt.setString(2, eagles.getPosit());
+			pStmt.setString(3, eagles.getHs());
+			pStmt.setString(4, eagles.getHand());
+			pStmt.setString(5, eagles.getNickname());
+			pStmt.setInt(6, eagles.getBackNo());
+			pStmt.setString(7, eagles.getBirth());
+			pStmt.setInt(8, eagles.getSalary());
+			pStmt.setInt(9, eagles.getNo());
+
+			pStmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pStmt != null && !pStmt.isClosed())
+					pStmt.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+	}
+
+	public void deleteEagles(int no) {
+		String query = "delete from eagles where no=?;";
+		PreparedStatement pStmt = null;
+		try {
+			pStmt = conn.prepareStatement(query);
+			pStmt.setInt(1, no);
+
+			pStmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pStmt != null && !pStmt.isClosed())
+					pStmt.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+	}
+
+	public void close() {
+		try {
+			if (conn != null && !conn.isClosed())
+				conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ MySQL/src/basic03/EaglesDTO.java
@@ -0,0 +1,114 @@
+package basic03;
+
+public class EaglesDTO {
+	private int no;
+	private String name;
+	private String posit;
+	private String hs;
+	private String hand;
+	private String nickname;
+	private int backNo;
+	private String birth;
+	private int salary;
+
+	public EaglesDTO(int no, String name, String posit, String hs, String hand, String nickname, int backNo, String birth,
+			int salary) {
+		this.no = no;
+		this.name = name;
+		this.posit = posit;
+		this.hs = hs;
+		this.hand = hand;
+		this.nickname = nickname;
+		this.backNo = backNo;
+		this.birth = birth;
+		this.salary = salary;
+	}
+	
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public EaglesDTO(int no) {
+		super();
+		this.no = no;
+	}
+
+	public EaglesDTO() {
+
+	}
+
+	public int getBackNo() {
+		return backNo;
+	}
+
+	public void setBackNo(int backNo) {
+		this.backNo = backNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPosit() {
+		return posit;
+	}
+
+	public void setPosit(String posit) {
+		this.posit = posit;
+	}
+
+	public String getHs() {
+		return hs;
+	}
+
+	public void setHs(String hs) {
+		this.hs = hs;
+	}
+
+	public String getHand() {
+		return hand;
+	}
+
+	public void setHand(String hand) {
+		this.hand = hand;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "EaglesDTO [no=" + no + ", name=" + name + ", posit=" + posit + ", hs=" + hs + ", hand=" + hand
+				+ ", nickname=" + nickname + ", backNo=" + backNo + ", birth=" + birth + ", salary=" + salary + "]";
+	}
+
+}
