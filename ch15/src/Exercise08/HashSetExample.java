80ca4e9a110574fcf100ca6b3dfe6002c4228670
lx9203 <lx9203@naver.com>
2019-04-09 16:45:36 +0900

    Exercise08

---
+++ ch15/src/Exercise08/HashSetExample.java
@@ -0,0 +1,20 @@
+package Exercise08;
+
+import java.util.*;
+
+public class HashSetExample {
+	public static void main(String[] args) {
+		Set<Student> set = new HashSet<Student>();
+		
+		set.add(new Student(1, "홍길동"));
+		set.add(new Student(2, "신용권"));
+		set.add(new Student(1, "조민우"));
+		
+		Iterator<Student> iterator = set.iterator();
+		while(iterator.hasNext()) {
+			Student student = iterator.next();
+			System.out.println(student.studentNum + ":" + student.name);
+		}
+	}
+
+}
