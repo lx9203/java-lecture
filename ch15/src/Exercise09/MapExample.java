583c392e93c8249709a7dcfb27a948fbfe720a85
lx9203 <lx9203@naver.com>
2019-04-09 16:45:43 +0900

    Exercise09

---
+++ ch15/src/Exercise09/MapExample.java
@@ -0,0 +1,30 @@
+package Exercise09;
+
+import java.util.*;
+
+public class MapExample {
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("blue", 96);
+		map.put("hong", 86);
+		map.put("white", 92);
+
+		String name = null;
+		int maxScore = 0;
+		int totalScore = 0;
+
+		Set<Map.Entry<String, Integer>> entrySet = map.entrySet();
+		for (Map.Entry<String, Integer> entry : entrySet) {
+			if (entry.getValue() > maxScore) {
+				name = entry.getKey();
+				maxScore = entry.getValue();
+			}
+			totalScore += entry.getValue();
+		}
+		int avgScore = totalScore / map.size();
+		
+		System.out.println("평균 점수 : " + avgScore);
+		System.out.println("최고 점수 : " + maxScore);
+		System.out.println("최고 점수를 받은 아이디 : " + name);
+	}
+}
